7c7c5896d6c189db5614a72274e4048e11b141d5
dfyisco <50670460+dfyisco@users.noreply.github.com>
2020-12-27 15:02:32 +0000

    Add files via upload

---
+++ sum_of_2_numbers.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+"""
+class Solution:
+    def twoSum(self, nums, target: int):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                else:
+                    pass

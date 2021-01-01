ac6b089cc577c5fb4b68cac8da06745c19d88687
dfyisco <50670460+dfyisco@users.noreply.github.com>
2021-01-01 21:48:11 +0000

    Add files via upload

---
+++ longest_substr_without_rep.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  1 20:06:19 2021
+
+@author: ThinkPad
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        k, res, c_dict = -1, 0, {}
+        for i, c in enumerate(s):
+            if c in c_dict and c_dict[c] > k:  # 字符c在字典中 且 上次出现的下标大于当前长度的起始下标
+                k = c_dict[c]
+                c_dict[c] = i
+            else:
+                c_dict[c] = i
+                res = max(res, i-k)
+        return res

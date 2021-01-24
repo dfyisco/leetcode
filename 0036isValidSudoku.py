2ced235235c4a0fbbd9022b3b79983f503595afc
dfyisco <50670460+dfyisco@users.noreply.github.com>
2021-01-24 23:17:14 +0000

    Update 0036isValidSudoku.py

--- 0036isValidSudoku.py
+++ 0036isValidSudoku.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jan 24 23:15:27 2021
+判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
 
-@author: ThinkPad
+数字 1-9 在每一行只能出现一次。
+数字 1-9 在每一列只能出现一次。
+数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-sudoku
 """
 
 class Solution:
